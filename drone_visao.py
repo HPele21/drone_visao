1fbc58ebd4d54bc8b84bbbc132cc2817001e2a09
HPele21 <134882512+HPele21@users.noreply.github.com>
2024-07-19 00:06:05 -0300

    adiçao da limiarizaçao

--- drone_visao.py
+++ drone_visao.py
@@ -2,9 +2,24 @@ import cv2 #lib pro processamento imagem e video
 import numpy as np #lib pra manipular os arrays
 from pyzbar.pyzbar import decode #lib pra decodificar qrcode e codigo de barras
 
+# ===================================================================================================================================================================
+# QRcode / codigo de barras
+# ===================================================================================================================================================================
+
 cap = cv2.VideoCapture(0) #objeto para a captura e webcam id
 cap.set(3,640) #largura(640 pixels)
 cap.set(4,480) #altura(480 pixels)
+
+# ===================================================================================================================================================================
+# Limiarização
+# ===================================================================================================================================================================
+def preProcess(img):
+    imgPre = cv2.GaussianBlur(img,(5,5),3)
+    imgPre = cv2.Canny(imgPre,90,140)
+    kernel = np.ones((4,4),np.uint8)
+    imgPre = cv2.dilate(imgPre,kernel,iterations = 2)
+    imgPre = cv2.erode(imgPre,kernel,iterations = 2)
+    return imgPre
 
 while True: #loop
 
@@ -16,9 +31,12 @@ while True: #loop
         print(myData)
         pts = np.array([barcode.polygon], np.int32) #obtem os pontos e cria o array
         pts = pts.reshape((-1,1,2))
-        cv2.polylines(img,[pts],True,(255,0,255),5) #define a imagem, os pontos, a cor e espessura
+        cv2.polylines(img,[pts],True,(0,255,0),5) #define a imagem, os pontos, a cor e espessura
         pts2 = barcode.rect #posiçao do poligono
-        cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(255,0,255),2) #colocando o conteudo na borda
+        cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(0,255,0),2) #colocando o conteudo na borda
 
     cv2.imshow('Result', img) #mostra a imagem
     cv2.waitKey(1) #delay
+
+    imgPre = preProcess(img)
+    cv2.imshow('IMG', imgPre)
